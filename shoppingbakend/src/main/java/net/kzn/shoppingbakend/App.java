186278261844ac4dd96495b7b0d180e920b078fc
raorohan <rohanrao.0110@gmail.com>
2018-05-29 21:45:07 +0530

    My Project Setup Completed.

---
+++ shoppingbakend/src/main/java/net/kzn/shoppingbakend/App.java
@@ -0,0 +1,13 @@
+package net.kzn.shoppingbakend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
